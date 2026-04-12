287ac29a1316c8d0513947d30fdf346ab5b1aa93
galitz-matt <galitz.matthew@gmail.com>
2026-04-12 18:44:01 -0400

    fix: prevent empty pi names

--- src/domain/mapper/ir-to-dto.mapper.ts
+++ src/domain/mapper/ir-to-dto.mapper.ts
@@ -3,11 +3,12 @@ import type { NameCriteria } from "../types/request";
 
 export class IrToDtoMapper {
     static toNameCriteria(piName: PiName): NameCriteria {
-        return {
-            any_name: piName.anyName,
-            first_name: piName.firstName,
-            last_name: piName.lastName,
-            middle_name: piName.middleName
-        };
+        const res: NameCriteria = {};
+        if (piName.anyName !== undefined) res.any_name = piName.anyName;
+        if (piName.firstName !== undefined) res.first_name = piName.firstName;
+        if (piName.lastName !== undefined) res.last_name = piName.lastName;
+        if (piName.middleName !== undefined) res.middle_name = piName.middleName;
+
+        return res;
     }
 }
