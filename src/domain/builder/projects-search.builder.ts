c491368417dbe2c10941d7e8c0c3840df69da5dc
galitz-matt <galitz.matthew@gmail.com>
2026-04-12 18:15:18 -0400

    feat: fiscalYears, includeActiveProjects, piNames builder functions

--- src/domain/builder/projects-search.builder.ts
+++ src/domain/builder/projects-search.builder.ts
@@ -1,10 +1,13 @@
 import { DomainError } from "../errors";
 import { DefaultsFactory } from "../factory/defaults.factory";
+import { IrToDtoMapper } from "../mapper/ir-to-dto.mapper";
 import { Field } from "../types/field";
+import type { PiName } from "../types/ir";
 import {
     SortOrder,
     type ProjectsSearchRequest
 } from "../types/request"
+import { PiNameBuilder } from "./pi-name.builder";
 
 export class ProjectsSearchBuilder {
     private request: ProjectsSearchRequest;
@@ -58,7 +61,6 @@ export class ProjectsSearchBuilder {
     includeFields(...fields: Field[]): this {
         const excludedFields = new Set(this.request.exclude_fields ?? []);
         const conflicts = fields.filter(f => excludedFields.has(f));
-
         if (conflicts.length !== 0) {
             throw new DomainError(
                 `Cannot include excluded fields:\n${this.formatList(conflicts)}` +
@@ -77,10 +79,9 @@ export class ProjectsSearchBuilder {
     excludeFields(...fields: Field[]): this {
         const includedFields = new Set(this.request.include_fields ?? []);
         const conflicts = fields.filter(f => includedFields.has(f));
-        
         if (conflicts.length !== 0) {
             throw new DomainError(
-                `Cannot exclude included fields:\n${this.formatList(conflicts)}` +
+                `Cannot exclude included fields:\n${this.formatList(conflicts)}\n` +
                 `Remove them from includeFields or excludeFields`
             )
         }
@@ -89,9 +90,63 @@ export class ProjectsSearchBuilder {
         return this;
     }
 
-    
+    /**
+     * Filter results by fiscal year appropriation from which project funds were obligated
+     * @param years - fiscal years
+     */
+    fiscalYears(...years: number[]): this {
+        const conflicts = years.filter(y => 
+            !Number.isInteger(y)
+        );
+        if (conflicts.length !== 0) {
+            throw new DomainError(
+                `Cannot filter by fiscal years:\n${this.formatList(conflicts)}\n` +
+                `Fiscal years must be integers`
+            )
+        }
 
-    private formatList(fields: string[]): string {
+        this.request.criteria.fiscal_years = years;
+        return this;
+    }
+
+    /**
+     * Include active projects (project whose latest Budget End Date has not occurred yet) that match criteria.
+     * This expands results and may override filters such as `fiscal_years`
+     * @param - true/false
+     */
+    includeActiveProjects(include: boolean): this {
+        this.request.criteria.include_active_projects = include;
+        return this;
+    }
+
+    /**
+     * Filters projects by Principal Investigator (PI) names.
+     *
+     * Each provided name is converted into API-compatible criteria.
+     * See {@link PiName} for matching behavior and field semantics.
+     * 
+     * Example usage:
+     * 
+     * ```
+     * nih.projects
+     *    .piNames(
+     *        pi().firstName("Christopher").lastName("Moltisanti"),
+     *        { firstName: "Corrado", middleName: "Junior" },
+     *        pi().anyName("Gualtieri")
+     *    )
+     * ```
+     */
+    piNames(...names: (PiName | PiNameBuilder)[]): this {
+        this.request.criteria.pi_names = names.map(n => {
+            n = n instanceof PiNameBuilder
+                ? n.build()
+                : n;
+            return IrToDtoMapper.toNameCriteria(n)
+        });
+        return this;
+    }
+
+    private formatList(fields: (string | number)[]): string {
         return fields.map(f => ` - ${f}`).join("\n");
     }
 }
