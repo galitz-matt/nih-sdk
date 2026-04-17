a6a3d24934b3f17135b7b34261f6eba4ea52326d
galitz-matt <galitz.matthew@gmail.com>
2026-04-17 16:00:45 -0400

    refactor: rename

--- src/domain/mapper/dto-to-model.mapper.ts
+++ src/domain/mapper/dto-to-model.mapper.ts
@@ -1,14 +1,13 @@
-import type { NameCriteriaIr } from "../types/ir";
-import type { NameCriteria } from "../types/request";
+import type { NameCriteria } from "../types/model/request";
+import type { NameCriteriaDto } from "../types/dto/name-criteria.dto";
 
-export class IrToDtoMapper {
-    static toNameCriteria(piName: NameCriteriaIr): NameCriteria {
+export class DtoToModelMapper {
+    static toNameCriteria(piName: NameCriteriaDto): NameCriteria {
         const res: NameCriteria = {};
-        if (piName.anyName !== undefined) res.any_name = piName.anyName;
+        if (piName.anyName !== undefined) res.any_name;
         if (piName.firstName !== undefined) res.first_name = piName.firstName;
         if (piName.lastName !== undefined) res.last_name = piName.lastName;
         if (piName.middleName !== undefined) res.middle_name = piName.middleName;
-
         return res;
     }
 }
