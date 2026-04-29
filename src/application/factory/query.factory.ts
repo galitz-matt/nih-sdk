2c6ae5257d14d1ad605ce0ae14481dba4fc30d2c
galitz-matt <galitz.matthew@gmail.com>
2026-04-28 20:51:36 -0400

    chore: clean imports

--- src/application/factory/query.factory.ts
+++ src/application/factory/query.factory.ts
@@ -1,8 +1,6 @@
 import type { ProjectsBuilder } from "../../domain/builder/projects.builder"
-import type { ProjectsOutput } from "../../domain/types/model/projects-output.model"
 import type { ProjectsClient } from "../../infra/nih/projects.client"
 import type { ProjectQuery } from "../types/project.query"
-import type { Executable } from "../types/executable"
 
 export class QueryFactory {
     static createProjectsQuery(
