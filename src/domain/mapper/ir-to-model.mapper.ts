c5ace59c0fdcab1a5386dd5a15b4c36d8c7ba198
galitz-matt <galitz.matthew@gmail.com>
2026-05-11 21:11:27 -0400

    fix: init model property

--- src/domain/mapper/ir-to-model.mapper.ts
+++ src/domain/mapper/ir-to-model.mapper.ts
@@ -4,7 +4,7 @@ import type { NameCriteriaIr } from "../types/ir/name-criteria.ir";
 export class IrToModelMapper {
     static toNameCriteria(piName: NameCriteriaIr): NameCriteria {
         const res: NameCriteria = {};
-        if (piName.anyName !== undefined) res.any_name;
+        if (piName.anyName !== undefined) res.any_name = piName.anyName;
         if (piName.firstName !== undefined) res.first_name = piName.firstName;
         if (piName.lastName !== undefined) res.last_name = piName.lastName;
         if (piName.middleName !== undefined) res.middle_name = piName.middleName;
