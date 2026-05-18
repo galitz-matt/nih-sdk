03c7c7fdb0d9599d749595af066331e5e189b90b
galitz-matt <galitz.matthew@gmail.com>
2026-05-18 19:46:30 -0400

    feat: ApiItemWithParentValue type

--- scripts/types.ts
+++ scripts/types.ts
@@ -8,3 +8,15 @@ export type ApiItemWithChildren = {
   value: string | null;
   children_values: string[] | null;
 };
+
+export type ApiItemWithParent = {
+  name: string | null;
+  display: string | null;
+  value: string | null;
+  parent_value: string | null;
+  properties: Properties
+}
+
+type Properties = {
+  Description: string | null
+} | null
