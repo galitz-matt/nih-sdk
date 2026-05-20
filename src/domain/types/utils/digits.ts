924ce91be5cdb7e0bacd86535e5da7e72c9298ed
galitz-matt <galitz.matthew@gmail.com>
2026-05-19 21:11:23 -0400

    feat: digit types

---
+++ src/domain/types/utils/digits.ts
@@ -0,0 +1,5 @@
+export type TwoDigit = `${number}${number}`;
+
+export type FourDigit = `${number}${number}${number}${number}`;
+
+export type SixDigit = `${number}${number}${number}${number}${number}${number}`;
