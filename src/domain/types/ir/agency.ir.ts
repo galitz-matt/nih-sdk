b59ea19894eecd717a314c39dadc197658d85660
galitz-matt <galitz.matthew@gmail.com>
2026-05-21 19:39:49 -0400

    feat: agencyIr

---
+++ src/domain/types/ir/agency.ir.ts
@@ -0,0 +1,8 @@
+import type { Agency } from "../enum/agency"
+import type { NonEmptyArray } from "../utils/non-empty"
+
+export type AgencyIr = {
+    agencies: NonEmptyArray<Agency>,
+    isAdministering: boolean,
+    isFunding: boolean
+}
