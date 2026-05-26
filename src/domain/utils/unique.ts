77ad865d14d421a7c06926ced45f12540f3523eb
galitz-matt <galitz.matthew@gmail.com>
2026-05-26 19:11:04 -0400

    refactor: delete unused type

--- src/domain/utils/unique.ts
+++ src/domain/utils/unique.ts
@@ -1,11 +1,5 @@
-import type { NonEmptyArray } from "../types/utils/non-empty";
-
 export function unique<T>(values: readonly T[]): T[] {
     return [...new Set(values)]
-}
-
-export function uniqueNonEmpty<T>(values: NonEmptyArray<T>): NonEmptyArray<T> {
-    return unique(values) as NonEmptyArray<T>
 }
 
 export function uniqueFlat<T>(...values: readonly (T | readonly T[])[]): T[] {
