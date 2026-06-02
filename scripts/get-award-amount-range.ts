32948fcb5fd1012a658b8a66263a7719d49017cd
galitz-matt <galitz.matthew@gmail.com>
2026-06-01 20:34:55 -0400

    feat: script to generate award amount range constants

---
+++ scripts/get-award-amount-range.ts
@@ -0,0 +1,44 @@
+import { line, render, seq } from "@galitz-matt/ts-struct";
+import { BASE_URLS } from "../src/infra/config";
+import { join } from "path"
+
+async function main() {
+    const URL = BASE_URLS.WEBAPP + "/services/Lookup/awardAmountRange";
+    
+    const res = await Bun.fetch(URL);
+    const raw = await res.json();
+
+    if (typeof raw !== "object" || raw === null) {
+        throw new Error("Unexpected response: not an object or is null");
+    }
+
+    if (
+        !("min_award_amount" in raw) ||
+        !("max_award_amount" in raw)
+    ) {
+        throw new Error("Unexpected response: required props are missing");
+    }
+
+    if (
+        typeof raw.max_award_amount !== "number" ||
+        typeof raw.min_award_amount !== "number"
+    ) {
+        throw new Error("Unexpected response: invalid values");
+    }
+
+    const output = render(seq(
+        line(`const MIN_AWARD_AMOUNT = ${raw.min_award_amount};`),
+        line(`const MAX_AWARD_AMOUNT = ${raw.max_award_amount};`)
+    ))
+
+    const output_path = join(
+        import.meta.dir,
+        "../src/domain/constants/amount-range.ts"
+    );
+
+    Bun.write(output_path, output);
+
+    console.log("Generated award amount range.");
+}
+
+main();
