1e0ca763dd2fb3aeca5a00f55eeb48c5ad26df80
galitz-matt <galitz.matthew@gmail.com>
2026-06-11 20:08:58 -0400

    feat: formatting

--- src/infra/nih/projects-query.result.ts
+++ src/infra/nih/projects-query.result.ts
@@ -2,16 +2,21 @@ import type { ProjectsOutput } from "../types/model/projects-output.model";
 
 export class ProjectsQueryResult {
     readonly response: ProjectsOutput;
+    private defaultIndent = 4;
 
     constructor(response: ProjectsOutput) {
         this.response = response;
     }
 
-    toTextFile(path: string): void {
+    toTextFile(path: string, indent: number | string = this.defaultIndent): void {
         Bun.write(path, this.toText());
     }
 
-    toText(): string {
-        return JSON.stringify(this.response);
+    toText(indent: number | string = this.defaultIndent): string {
+        return JSON.stringify(
+            this.response,
+            null,
+            indent
+        );
     }
 }
